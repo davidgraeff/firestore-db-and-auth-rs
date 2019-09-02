a25a7c057fdc178564d449bc5976a1f90fc15c3d
David Graeff <david.graeff@web.de>
2019-09-02 20:21:48 +0200

    Github Actions: Requires environment variables to be assigned
    Signed-off-by: David Graeff <david.graeff@web.de>

--- ci.sh
+++ ci.sh
@@ -2,6 +2,4 @@
 [[ $(fgrep -r 'println' src/ | wc -l) -eq 0 ]] || { echo >&2 'Left over println!'; exit 1; }
 echo $SERVICE_ACCOUNT_JSON | base64 -d > firebase-service-account.json
 head -n 3 firebase-service-account.json
-echo "  ---  "
-echo $SERVICE_ACCOUNT_JSON | head -c 50
 [[ $(jq -r ".auth_uri" firebase-service-account.json) == "https://accounts.google.com/o/oauth2/auth" ]] || { echo >&2 'Failed to extract firebase-service-account.json'; exit 1; }
