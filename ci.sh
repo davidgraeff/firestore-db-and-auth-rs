a5691f182a972d72a63446ec66a463ea81e36c47
David Graeff <david.graeff@web.de>
2019-09-02 19:38:01 +0200

    Remove cargo.lock. Tweak ci.sh
    Signed-off-by: David Graeff <david.graeff@web.de>

--- ci.sh
+++ ci.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 [[ $(fgrep -r 'println' src/ | wc -l) -eq 0 ]] || { echo >&2 'Left over println!'; exit 1; }
 echo $SERVICE_ACCOUNT_JSON | base64 -d > firebase-service-account.json
+[[ $(jq -r ".auth_uri" firebase-service-account.json) == "https://accounts.google.com/o/oauth2/auth" ]] || { echo >&2 'Failed to extract firebase-service-account.json'; exit 1; }
