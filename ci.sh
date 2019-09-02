f15549bcebc52eaf58127e0204e8085da0d085fe
David Graeff <david.graeff@web.de>
2019-09-02 20:53:41 +0200

    Github Actions: Mac OS base64 uses a different command line option
    Signed-off-by: David Graeff <david.graeff@web.de>

--- ci.sh
+++ ci.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
 [[ $(fgrep -r 'println' src/ | wc -l) -eq 0 ]] || { echo >&2 'Left over println!'; exit 1; }
-echo $SERVICE_ACCOUNT_JSON | base64 -d > firebase-service-account.json
+if [ "$(uname)" == "Darwin" ]; then
+    echo $SERVICE_ACCOUNT_JSON | base64 -D > firebase-service-account.json
+else
+    echo $SERVICE_ACCOUNT_JSON | base64 -d > firebase-service-account.json
+fi
 head -n 3 firebase-service-account.json
 [[ $(jq -r ".auth_uri" firebase-service-account.json) == "https://accounts.google.com/o/oauth2/auth" ]] || { echo >&2 'Failed to extract firebase-service-account.json'; exit 1; }
