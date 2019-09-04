68e44ceb47f8eb221d93920b96899f0833b237ca
David Graeff <david.graeff@web.de>
2019-09-04 22:12:42 +0200

    Improve debug println check
    Signed-off-by: David Graeff <david.graeff@web.de>

--- tests/left_over_println.sh
+++ tests/left_over_println.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -e
-[[ $(fgrep -r 'println' src/ | wc -l) -eq 0 ]] || { echo >&2 'Please remove all debug println!'; exit 1; }
+[[ $(fgrep -n -r "println" src/ | grep -v  "///" | wc -l) -eq 0 ]] || { echo >&2 'Please remove all debug println'; fgrep -n -r "println" src/ | grep -v  "///"; exit 1; }
