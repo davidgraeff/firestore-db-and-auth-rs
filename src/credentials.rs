203363eebb6c4cc1480ef7634fbd5817890d16d1
Michał Kawalec <michal@monad.cat>
2021-12-06 15:52:01 +0100

    removed the expiration messages

--- src/credentials.rs
+++ src/credentials.rs
@@ -262,7 +262,6 @@ impl Credentials {
             max_age_public
         };
 
-        println!("Google JWKs expires at: {:?}", expires_at);
         keys.pub_key_expires_at = Some(offset::Utc::now() + expires_at);
         Ok(())
     }
